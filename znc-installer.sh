4438b90f8546eab3b1b4ed16758b32eb71117a22
TRAGiDY <justin@ejeet.net>
2016-07-20 13:08:53 -0230

    Update znc-installer.sh

--- znc-installer.sh
+++ znc-installer.sh
@@ -34,6 +34,15 @@ wait $!
 tar -xzvf znc*.*gz &
 wait $!
 cd znc*
+echo ""
+	echo "Please select a configuration option"
+	echo "   1) Standard Build and Installtion"
+	echo "   2) Low Memory Build and Installation"
+	read -p "CONFOP [1-2]: " -e -i 1 CONFOP
+	echo ""
+	
+	case $VID in
+		1)
 echo "Configuring ZNC with basic/stock options"
 ./configure >/dev/null 2>&1 &
 wait $!
@@ -47,6 +56,23 @@ echo "Installing ZNC to system"
 wait $!
 echo "Build Complete!"
 echo "Under any account on the system please issue the command znc --makeconf to create a znc client for this user."
+		;;
+		2)
+echo "Configuring ZNC with basic/stock options"
+./configure >/dev/null 2>&1 &
+wait $!
+echo "Configure complete, getting read to build from source"
+echo "Building from source... This may take some time please wait"
+make >/dev/null 2>&1 &
+wait $!
+echo "Build Complete!"
+make install >/dev/null 2>&1 &
+echo "Installing ZNC to system"
+wait $!
+echo "Build Complete!"
+echo "Under any account on the system please issue the command znc --makeconf to create a znc client for this user."
+		;;
+	esac
 
 
 
