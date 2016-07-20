d2270c23f8c47d460aa5cc2fd1d9a3cc7e58c456
TRAGiDY <justin@ejeet.net>
2016-07-20 13:04:58 -0230

    Rough Script
    Untested, expect issues.

--- znc-installer.sh
+++ znc-installer.sh
@@ -1,2 +1,52 @@
+#!/bin/bash
 # ZNC INSTALLER from Source
+# By TRAGiDY https://github.com/Tragidy/
 # Downloads, Compiles and Installs ZNC
+
+# Clear window, show banner credits
+clear
+
+# Check for root
+if [[ "$EUID" -ne 0 ]]; then
+	echo "Sorry, you need to run this as root"
+	exit 1
+fi
+
+if [[ -e /etc/debian_version ]]; then
+	OS=debian
+	RCLOCAL='/etc/rc.local'
+elif [[ -e /etc/centos-release || -e /etc/redhat-release ]]; then
+	OS=centos
+	RCLOCAL='/etc/rc.d/rc.local'
+	# Needed for CentOS 7
+	chmod +x /etc/rc.d/rc.local
+else
+	echo "Looks like you aren't running this installer on a Debian or CentOS system"
+	exit 4
+fi
+
+# Create a clean working space
+rm -rf zncinstaller
+mkdir zncinstaller
+cd zncinstaller
+wget http://znc.in/releases/znc-1.6.3.tar.gz &
+wait $!
+tar -xzvf znc*.*gz &
+wait $!
+cd znc*
+echo "Configuring ZNC with basic/stock options"
+./configure >/dev/null 2>&1 &
+wait $!
+echo "Configure complete, getting read to build from source"
+echo "Building from source... This may take some time please wait"
+make >/dev/null 2>&1 &
+wait $!
+echo "Build Complete!"
+make install >/dev/null 2>&1 &
+echo "Installing ZNC to system"
+wait $!
+echo "Build Complete!"
+echo "Under any account on the system please issue the command znc --makeconf to create a znc client for this user."
+
+
+
